1b25fdc682dd16d925f00ef9ffa89fd5f83d5963
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-04-07 01:27:14 +0700

    register form

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
-  return <div>Register</div>;
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = () => {};
+
+  const registerForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        className='form-control'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        autoFocus
+      />
+
+      <button type='submit' className='btn btn-primary'>
+        Register
+      </button>
+    </form>
+  );
+
+  return (
+    <div className='container p-5'>
+      <div className='row'>
+        <div className='col-md-6 offset-md-3'>
+          <h4>Register</h4>
+          {registerForm()}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
