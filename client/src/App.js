b1d0e16093a834ffc000012f5c587ee11007c2dd
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-04 15:25:35 +0700

    setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,7 @@
-import { Routes, Route } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-import Register from './pages/auth/Register';
-import Login from './pages/auth/Login';
-import Home from './pages/Home';
-import Header from './components/nav/Header';
+import './App.css';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <ToastContainer />
-      <Routes>
-        <Route path='/' exact element={<Home />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/register' element={<Register />}></Route>
-      </Routes>
-    </>
-  );
+  return <div>Hello World</div>;
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import 'antd/dist/antd';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  // </React.StrictMode>
-);
+root.render(<App />);
