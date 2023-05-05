664b671ecaf4430303ca60d6add2d5c6110ae4b2
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-05 18:30:44 +0700

    complete registration

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Home from './pages/Home';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Header from './components/nav/Header';
+import RegisterComplete from './pages/auth/RegisterComplete';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route exact path='/' element={<Home />} />
         <Route exact path='/login' element={<Login />} />
         <Route exact path='/register' element={<Register />} />
+        <Route exact path='/register/complete' element={<RegisterComplete />} />
         <Route exact path='*' element={<h2>Not Found</h2>} />
       </Routes>
     </>
