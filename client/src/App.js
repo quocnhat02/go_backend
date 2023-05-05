a29ce012dd3313f9ef2c35b6a0228ac3ecbce6e9
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-05 19:02:21 +0700

    user in redux state

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,32 @@ import Register from './pages/auth/Register';
 import Header from './components/nav/Header';
 import RegisterComplete from './pages/auth/RegisterComplete';
 
+import { auth } from './firebase';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult();
+
+        dispatch({
+          type: 'LOGGED_IN_USER',
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        });
+      }
+    });
+
+    // cleanup
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <Header />
