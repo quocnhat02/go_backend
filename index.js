f7801020aeca39dc1b8c78adcaa6f5d54e933c75
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-04-28 16:22:31 +0700

    ECDH AES

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ const aliceSharedKey = alice.computeSecret(bobPublicKeyBase64, 'base64', 'hex');
 const bobSharedKey = alice.computeSecret(alicePublicKeyBase64, 'base64', 'hex');
 
 console.log(aliceSharedKey === bobSharedKey);
+
+const aes256 = require('aes256');
+
+const message = 'This is random message';
+
+const encrypted = aes256.encrypt(aliceSharedKey, message);
+
+const decrypted = aes256.decrypt(bobSharedKey, encrypted);
+
+console.log('encrypted: ', encrypted);
+console.log(decrypted);
