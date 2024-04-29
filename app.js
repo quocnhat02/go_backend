9e208de1b12e16f2d94aaff6557bac9105274261
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-04-29 21:48:23 +0700

    single upload file 1

--- app.js
+++ app.js
@@ -1,45 +1,14 @@
 const express = require('express');
-const morgan = require('morgan');
-const createError = require('http-errors');
-const authRoute = require('./routes/auth.route');
-const { verifyAccessToken } = require('./helpers/jwt_helper');
-
-require('dotenv').config();
-require('./helpers/init_mongodb');
-require('./helpers/init_redis').connect();
+const fileUpload = require('express-fileupload');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.set('view engine', 'ejs');
 
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-app.use(morgan('dev'));
-
-app.get('/', verifyAccessToken, async (req, res, next) => {
-  res.json('Hello from express.');
+app.get('/', (req, res, next) => {
+  res.render('index');
 });
 
-app.use('/auth', authRoute);
-
-app.use(async (req, res, next) => {
-  next(createError.NotFound());
-});
-
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.send({
-    error: {
-      status: err.status || 500,
-      message: err.message,
-    },
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
 });
