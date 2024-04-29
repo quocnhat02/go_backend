e7671185e39baa673b6ba3b8f152b9c55ac30cea
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-04-29 21:34:35 +0700

    clean code

---
+++ controllers/auth.controller.js
@@ -0,0 +1,109 @@
+const createError = require('http-errors');
+const User = require('../models/user.model');
+const { authSchema } = require('../helpers/validation_schema');
+const {
+  signAccessToken,
+  signRefreshToken,
+  verifyRefreshToken,
+} = require('../helpers/jwt_helper');
+const client = require('../helpers/init_redis');
+
+module.exports = {
+  register: async (req, res, next) => {
+    try {
+      const result = await authSchema.validateAsync(req.body);
+
+      const doesExist = await User.findOne({ email: result.email });
+      if (doesExist) {
+        throw createError.Conflict(
+          `${result.email} is already been registered.`
+        );
+      }
+
+      const user = new User(result);
+      const savedUser = await user.save();
+      const userId = JSON.stringify(savedUser._id);
+      const accessToken = await signAccessToken(userId);
+      const refreshToken = await signRefreshToken(userId);
+
+      res.json({
+        accessToken,
+        refreshToken,
+      });
+    } catch (error) {
+      if (error.isJoi === true) error.status = 422;
+      next(error);
+    }
+  },
+
+  login: async (req, res, next) => {
+    try {
+      const result = await authSchema.validateAsync(req.body);
+      const user = await User.findOne({ email: result.email });
+
+      if (!user) {
+        throw createError.NotFound('User not registered.');
+      }
+
+      const isMatch = await user.isValidPassword(result.password);
+      if (!isMatch) {
+        throw createError.Unauthorized('Username/Password not valid.');
+      }
+
+      const userId = JSON.stringify(user._id);
+      const accessToken = await signAccessToken(userId);
+      const refreshToken = await signRefreshToken(userId);
+
+      res.json({
+        accessToken,
+        refreshToken,
+      });
+    } catch (error) {
+      if (error.isJoi === true) {
+        return next(createError.BadRequest('Invalid Username/Password.'));
+      }
+      next(error);
+    }
+  },
+
+  refreshToken: async (req, res, next) => {
+    try {
+      const { refreshToken } = req.body;
+      if (!refreshToken) {
+        throw createError.BadRequest();
+      }
+
+      const userId = await verifyRefreshToken(refreshToken);
+
+      const newAccessToken = await signAccessToken(userId);
+      const newRefreshToken = await signRefreshToken(userId);
+
+      res.json({ newAccessToken, newRefreshToken });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  logout: async (req, res, next) => {
+    try {
+      const { refreshToken } = req.body;
+      if (!refreshToken) {
+        throw createError.BadRequest();
+      }
+
+      const userId = await verifyRefreshToken(refreshToken);
+      client
+        .DEL(userId)
+        .then((value) => {
+          console.log(value);
+          res.sendStatus(204);
+        })
+        .catch((err) => {
+          console.log(err.message);
+          throw createError.InternalServerError();
+        });
+    } catch (error) {
+      next(error);
+    }
+  },
+};
