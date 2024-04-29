2260a65df94662cf2e0a94fa0cf4d92d2b12763c
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-04-29 16:36:05 +0700

    handling Jwt errors

--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -7,7 +7,7 @@ module.exports = {
       const payload = {};
       const secret = process.env.ACCESS_TOKEN_SECRET;
       const options = {
-        expiresIn: '1h',
+        expiresIn: '15s',
         issuer: 'lightcodese.com',
         audience: userId,
       };
@@ -30,7 +30,9 @@ module.exports = {
     const token = bearerToken[1];
     Jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
       if (err) {
-        return next(createError.Unauthorized());
+        const message =
+          err.name === 'JsonWebTokenError' ? 'Unauthorized' : err.message;
+        return next(createError.Unauthorized(message));
       }
       req.payload = payload;
       next();
