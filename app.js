1a9a2e522ab3fe2d289fb7cb9f6123a0baf741de
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-04-29 11:06:07 +0700

    define routes auth

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
 const createError = require('http-errors');
+const authRoute = require('./routes/auth.route');
 
 require('dotenv').config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use('/auth', authRoute);
 
 app.get('/', async (req, res, next) => {
   res.send('Hello from express.');
