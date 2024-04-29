c876cf55ea51c7ca4a4eb53585f44f07f2934a7f
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-04-29 10:58:50 +0700

    setup project and impl route and error

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const createError = require('http-errors');
+
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', async (req, res, next) => {
+  res.send('Hello from express.');
+});
+
+app.use(async (req, res, next) => {
+  next(createError.NotFound());
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
